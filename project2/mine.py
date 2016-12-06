074ffab0aadc391c530347340d225712ef6d5397
Chris Spencer <chrisspencer1013@gmail.com>
2016-12-06 14:19:33 -0500

    imlazy

--- project2/mine.py
+++ project2/mine.py
@@ -3,7 +3,11 @@ import re
 import csv
 from matplotlib import pyplot
 import numpy
-
+import nltk
+from textblob import TextBlob as TB
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from afinn import afinn_dic
 
 def word_in_text(word, text):
     word = word.lower()
@@ -18,11 +22,12 @@ tweet_id=[]
 tw_text=[]
 tw_language=[]
 tw_time=[]
+tw_sentiment=[]
 
-term_x=[]
-term_y=[]
-lang_x=[]
-lang_y=[]
+# term_x=[]
+# term_y=[]
+# lang_x=[]
+# lang_y=[]
 
 with open("tweets3.csv") as csv_file_in:
     reader = csv.DictReader(csv_file_in)
@@ -31,7 +36,10 @@ with open("tweets3.csv") as csv_file_in:
         tw_text.append(row[' tweet_text'])
         tw_language.append(row[' tweet_language'])
         tw_time.append(row[' tweet_time'])
-        
+        tw_sentiment.append(0)
+
+#tweets = [tweet_id, tw_text, tw_language, tw_time]
+
 cross_search = {        \
     'islam':0,          \
     'muslim':0,         \
@@ -87,6 +95,17 @@ cross_search = {        \
     'nazi':0,           \
     'fascis':0,         \
     'socialis':0,       \
+    'sexist':0,         \
+    'vote':0,           \
+    'popular vote':0,   \
+    'riot':0,           \
+    'electoral':0,      \
+    'MAGA':0,           \
+    'climate change':0, \
+    'cabinet':0,        \
+    'establishment':0,  \
+    'deport':0,         \
+    'america':0,        \
 }
 
 country = {
@@ -132,9 +151,13 @@ country = {
 
 
 for text in tw_text:
+    # for key, value in afinn.items():
+    #     if word_in_text(key, text):
+    #         tw_sentiment = 
     for key, value in cross_search.items():
         if word_in_text(key, text):
             cross_search[key]+=1
+
 for lang in tw_language:
     for key, value in country.items():
         if word_in_text(key, lang):
@@ -144,15 +167,66 @@ print("\nTotal Tweets:", len(tweet_id))
 
 print('\nKeyword count:')
 for key, value in cross_search.items():
-    term_y.append(value)
-    term_x.append(key)
+    # term_y.append(value)
+    # term_x.append(key)
     print('\t',key, value)
 
 print('\nGroup by country:')
 for key, value in country.items():
-    lang_y.append(value)
-    lang_x.append(key)
+    # lang_y.append(value)
+    # lang_x.append(key)
     print('\t',key, value)
 
+pos_tw = 0
+neg_tw = 0
+nue_tw = 0
+
+for text in tw_text:
+    blob = TB(text)
+    #print(text, blob.sentiment.polarity)
+    if (blob.sentiment.polarity > 0):
+        pos_tw +=1
+    elif (blob.sentiment.polarity < 0):
+        neg_tw +=1
+    else:
+        nue_tw +=1
+
+print('Positive Tweets (blob api):', pos_tw)
+print('Negative Tweets (blob api):', neg_tw)
+print('Nuetral Tweets (blob api):', nue_tw)
 
 
+#todo: track which hashtags are used together? - entities hashtags
+#todo: preprocessing stopwords, remove word endings?, take out puntuation
+#todo weighting based on retweets?
+#indico for sentiment API
+#setiment based on location
+#percentage retweets, hashtags per tweet (are people using hashtags?)
+#retweets over time
+#word cloud if there is time
+#print(tw_sentiment)
+words_found = 0
+for index, tweet in enumerate(tw_text):
+    for key, value in afinn_dic.items():
+        if(word_in_text(key, tweet)):
+            tw_sentiment[index] += value
+            words_found +=1
+            print('Found one!', words_found)
+    print('We are now on tweet number: ', index)
+
+
+pos_tw = 0
+neg_tw = 0
+nue_tw = 0
+
+for sen in tw_sentiment:
+    if sen > 0:
+        pos_tw +=1
+    elif sen < 0:
+        neg_tw +=1
+    else:
+        nue_tw +=1
+
+print('Positive Tweets (afinn method):', pos_tw)
+print('Negative Tweets (afinn method):', neg_tw)
+print('Nuetral Tweets (afinn method):', nue_tw)
